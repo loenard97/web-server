d8d17faf3d43ef85c2645ee3ec4d9008e972ff72
Dennis Lönard <dennis.loenard97@gmx.de>
2023-06-06 17:46:01 +0200

    moved http handling into new lib

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <div align="center">
 
 # web-server
-A web-server based on `The Rust Programming Language` Chapter 20
+A simple web-server to service HTML files
 
 ![](https://img.shields.io/github/last-commit/loenard97/web-server?&style=for-the-badge&color=F74C00)
 ![](https://img.shields.io/github/repo-size/loenard97/web-server?&style=for-the-badge&color=F74C00)
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,61 @@
 use std::io::prelude::*;
 use std::error::Error;
+use std::fs;
 use std::net::{TcpStream, TcpListener};
 
 mod thread_pool;
 use thread_pool::ThreadPool;
 
-mod http;
-use http::{Response, Request};
+use http_rs::request::Request;
+use http_rs::response::{Response, Status};
+
+/// Handle incoming requests
+/// 
+/// # Errors
+/// 
+/// Errors if handling request or sending response fails.
+fn handle_request(mut stream: &TcpStream) -> Result<(), Box<dyn Error>> {
+    /* parse request */
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer)?;
+    let message = String::from_utf8(buffer.to_vec()).unwrap();
+    let request = Request::from_string(message);
+
+    /* create response */
+    let html_path = handle_routing(request.path.unwrap());
+    let (content, status) = match fs::read_to_string(html_path.clone()) {
+        Ok(val) => (val, Status::Ok),
+        Err(_) => (fs::read_to_string("html/404.html")?, Status::NotFound),
+    };
+    let response = Response::new(Some(content), status);
+
+    /* send response */
+    stream.write(response.to_string().as_bytes())?;
+    stream.flush()?;
+
+    Ok(())
+}
+
+/// Get file path to html
+fn handle_routing(request_path: String) -> String {
+    let mut path = String::from("html");
+
+    if request_path == "/" {
+        path.push_str("/index.html");
+    } else {
+        path.push_str(&request_path);
+        path.push_str(".html");
+    }
+
+    path
+}
+
+/// Send error if request handling failed
+fn handle_failed_request(mut stream: &TcpStream) {
+    let response = Response::new(None, Status::NotFound);
+    stream.write(response.to_string().as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -22,31 +71,4 @@ fn main() {
             };
         });
     }
-}
-
-/// Handle incoming requests
-/// 
-/// # Errors
-/// 
-/// Errors if handling request or sending response fails.
-fn handle_request(mut stream: &TcpStream) -> Result<(), Box<dyn Error>> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer)?;
-
-    let request = Request::from_buffer(buffer);
-    println!("request {:?}", request);
-    let response = Response::from_request(request)?;
-
-    stream.write(response.text()?.as_bytes())?;
-    stream.flush()?;
-
-    Ok(())
-}
-
-/// Send error if request handling failed
-fn handle_failed_request(mut stream: &TcpStream) {
-    let response = Response::from_error();
-    let response_text = response.text().unwrap();
-    stream.write(response_text.as_bytes()).unwrap();
-    stream.flush().unwrap();
 }
